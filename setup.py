dfb1453a69c106d02f1461b9eeb544966a2ff4e6
Skyler Cain <skylercain@outlook.com>
2017-10-01 00:40:47 -0600

    Updated circleci pypi deploy commands

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='django-rest-framework-signature',
-    version='1.0.0.dev1',
+    version='1.0.1.dev1',
     description='Django Rest Framework Signature Authentication',
     author='Skyler Cain',
     author_email='skylercain@gmail.com',
