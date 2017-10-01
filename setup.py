aa7e436c3df2c1cc55345785de7165612a2ce482
Skyler Cain <skylercain@outlook.com>
2017-10-01 00:13:31 -0600

    Updated setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 
 setup(
     name='django-rest-framework-signature',
-    version='1.0.0',
-    description='Rent Dynamics Authentication',
+    version='1.0.0.dev1',
+    description='Django Rest Framework Signature Authentication',
     author='Skyler Cain',
     author_email='skylercain@gmail.com',
     url='https://github.com/Skylude/django-rest-framework-signature',
-    packages=['rest_framework_signature']
+    packages=['rest_framework_signature'],
+    keywords='django rest_framework authentication api_key'
 )
