e261c3e75e0b3dc8ae73b502d18267066e332c2e
Skyler Cain <skylercain@outlook.com>
2017-12-17 17:47:36 -0700

    Updated version number. Update includes multiple test projects in order to test different settings configurations. Has test project included for AWS cognito authentication

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='django-rest-framework-signature',
-    version='1.0.2.dev1',
+    version='1.0.3.dev1',
     description='Django Rest Framework Signature Authentication',
     author='Skyler Cain',
     author_email='skylercain@gmail.com',
     url='https://github.com/Skylude/django-rest-framework-signature',
     packages=['rest_framework_signature'],
-    keywords='django rest_framework authentication api_key'
+    keywords='django rest_framework authentication api_key signature'
 )
