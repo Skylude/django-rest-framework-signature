9bc7d1a55fa7d6fdec4d6ab0bbcd7b7f597501a1
Skyler Cain <skylercain@outlook.com>
2018-02-18 11:06:46 -0700

    Updated packages so the models folder will properly be published

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='django-rest-framework-signature',
-    version='1.1.0.dev1',
+    version='1.1.1.dev',
     description='Django Rest Framework Signature Authentication',
     author='Skyler Cain',
     author_email='skylercain@gmail.com',
     url='https://github.com/Skylude/django-rest-framework-signature',
-    packages=['rest_framework_signature'],
+    packages=['rest_framework_signature', 'rest_framework_signature.models'],
     keywords='django rest_framework authentication api_key signature'
 )
